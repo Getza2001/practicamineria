38f372eee603d6d2da92c2ef8a4b0675d03ebaa4
Getza2001 <193245@ids.upchiapas.edu.mx>
2022-06-29 19:32:30 -0500

    agregar proyecto

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const  fetch = require('node-fetch');
+
+
+router.get('/api', async (req, res) => {
+  var response = await fetch('https://api.agify.io?name=bella');
+  var data = await response.json();
+  return res.json(data);
+});
+
+module.exports = router;
